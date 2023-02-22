29a20621929a6ab749abce1931e808e8580bcddc
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-02-22 16:23:50 -0500

    a6 React.js Lab adding variable name

--- src/labs/a6/todo/todos.js
+++ src/labs/a6/todo/todos.js
@@ -1,5 +1,7 @@
-export default [
+const todos = [
     { "title": "Buy milk",          "status": "CANCELED",     "done": true   },
     { "title": "Pickup the kids",   "status": "IN PROGRESS",  "done": false  },
     { "title": "Walk the dog",      "status": "DEFERRED",     "done": false  }
 ];
+
+export default todos;
