dac4e3bd844c929e420c01d57bf4e54ee5de95a4
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-02-24 21:57:39 -0500

    a6 React.js Challenge

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,55 +1,56 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {useLocation} from "react-router";
 
-const NavigationSidebar = (
-    {
-        active = 'explore'
-    }
-) => {
+const NavigationSidebar = () => {
+    const {pathname} = useLocation();
+    const paths = pathname.split('/')
+    const active = paths[2];
     return (
         <div>
             <div className="list-group">
-                <a className="list-group-item" href="/">
+                <Link className="list-group-item"  to = "/">
                     <i className="fab fa-twitter"/>
-                </a>
-                <a className={`list-group-item ${active === "home" ? " active" : ""}`}
+                </Link>
+                <Link to = "/tuiter/home" className={`list-group-item ${active === "home" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-house"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Home</span>
-                </a>
-                <a className={`list-group-item ${active === "explore" ? " active" : ""}`}
+                </Link>
+                <Link to = "/tuiter/explore" className={`list-group-item ${active === "explore" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-hashtag"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Explore</span>
-                </a>
-                <a className={`list-group-item ${active === "notifications" ? "active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "notifications" ? "active" : ""}`}
       href="/">
                     <i className="fa-solid fa-bell"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Notifications</span>
-                </a>
-                <a className={`list-group-item ${active === "messages" ? "active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "messages" ? "active" : ""}`}
       href="/">
                     <i className="fa-solid fa-envelope"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Messages</span>
-                </a>
-                <a className={`list-group-item ${active === "bookmarks" ? " active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "bookmarks" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-bookmark"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Bookmarks</span>
-                </a>
-                <a className={`list-group-item ${active === "lists" ? " active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "lists" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-list"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Lists</span>
-                </a>
-                <a className={`list-group-item ${active === "profile" ? " active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "profile" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-user"/>
                     <span className="d-none d-xl-inline d-inline ps-2">Profile</span>
-                </a>
-                <a className={`list-group-item ${active === "more" ? " active" : ""}`}
+                </Link>
+                <Link to = "/" className={`list-group-item ${active === "more" ? " active" : ""}`}
       href="/">
                     <i className="fa-solid fa-ellipsis"/>
-                    <span className="d-none d-xl-inline d-inline ps-2">More</span></a>
+                    <span className="d-none d-xl-inline d-inline ps-2">More</span></Link>
             </div>
             <div className="d-grid mt-2">
                 <a href="/"
