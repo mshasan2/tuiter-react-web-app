f4f4ac19cd6a70926a51b12385342c4d49eff4f9
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2023-03-13 21:05:04 -0400

    a7 Redux Lab

---
+++ src/labs/a7/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReduxExamples from "./redux-examples";
+import Todos from "./redux-examples/todos-component";
+
+const Assignment7 = () => {
+    return(
+        <>
+            <h1>Assignment 7</h1>
+            <ReduxExamples/>
+        </>
+
+
+    );
+};
+
+export default Assignment7;
